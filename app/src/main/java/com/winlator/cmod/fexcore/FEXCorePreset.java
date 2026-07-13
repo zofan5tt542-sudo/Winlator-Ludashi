b13355f5ead165bad6be8eecf012577aa8b4e803
zofan5tt542-sudo <zofan5tt542@gmail.com>
2026-07-14 02:19:46 +0300

    Update FEXCorePreset.java

--- app/src/main/java/com/winlator/cmod/fexcore/FEXCorePreset.java
+++ app/src/main/java/com/winlator/cmod/fexcore/FEXCorePreset.java
@@ -7,6 +7,7 @@ public class FEXCorePreset {
     public static final String COMPATIBILITY = "COMPATIBILITY";
     public static final String INTERMEDIATE = "INTERMEDIATE";
     public static final String PERFORMANCE = "PERFORMANCE";
+    public static final String PERFORMANCE_TSO = "PERFORMANCE_TSO";
     public static final String CUSTOM = "CUSTOM";
     public final String id;
     public final String name;
