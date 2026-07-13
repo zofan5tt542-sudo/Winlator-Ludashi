5600675f6424b889105cfa88f4a8f629710d206d
zofan5tt542-sudo <zofan5tt542@gmail.com>
2026-07-14 02:34:26 +0300

    Update FEXCorePreset.java

--- app/src/main/java/com/winlator/cmod/fexcore/FEXCorePreset.java
+++ app/src/main/java/com/winlator/cmod/fexcore/FEXCorePreset.java
@@ -8,6 +8,7 @@ public class FEXCorePreset {
     public static final String INTERMEDIATE = "INTERMEDIATE";
     public static final String PERFORMANCE = "PERFORMANCE";
     public static final String PERFORMANCE_TSO = "PERFORMANCE_TSO";
+    public static final String DENUVO = "DENUVO";
     public static final String CUSTOM = "CUSTOM";
     public final String id;
     public final String name;
